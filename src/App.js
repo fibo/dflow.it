7b21e2196c842166ad8fac1c76b127ca581be75b
fibo <casati_gianluca@yahoo.it>
2020-11-06 20:32:53 +0100

    add footbar abd headbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo'
 
 export function App() {
   const { ref, width, height } = useResizeObserver()
+  const margin = 10
 
   return (
     <div className='app'>
@@ -12,7 +13,12 @@ export function App() {
         <Logo size={17} />
       </div>
       <div ref={ref} className='app__body'>
-        <DflowGraph dimension={{ width, height }} />
+        <DflowGraph
+          position={{ x: margin, y: 0 }}
+          dimension={{ width: width - margin * 2, height: height - margin }}
+          noFootbar
+          noHeadbar
+        />
       </div>
     </div>
   )
--- src/flow-view.js
+++ src/flow-view.js
@@ -1,16 +1,32 @@
 const origin = { x: 0, y: 0 }
 
-export function FlowViewNode({ children, position = origin, dimension }) {
+export function FlowViewNode({
+  children,
+  noFootbar = false,
+  noHeadbar = false,
+  position = origin,
+  dimension,
+}) {
   return (
     <div
       className='flow-view-node'
       style={{ ...dimension, top: position.y, left: position.x }}
     >
-      {children}
+      {noHeadbar || <div className='flow-view-node__headbar' />}
+
+      <div className='flow-view-node__body'>{children}</div>
+
+      {noFootbar || <div className='flow-view-node__footbar' />}
     </div>
   )
 }
 
-export function FlowViewCanvas(props) {
-  return <FlowViewNode {...props} />
+export function FlowViewCanvas({ nodes = [], ...props }) {
+  return (
+    <FlowViewNode {...props}>
+      {nodes.map((node, i) => (
+        <FlowViewNode key={i} {...node} />
+      ))}
+    </FlowViewNode>
+  )
 }
