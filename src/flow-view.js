e6894ba6ccfb7687b3263dec80a9c5c09e551221
fibo <casati_gianluca@yahoo.it>
2020-11-08 00:01:46 +0100

    can translate

--- src/flow-view.js
+++ src/flow-view.js
@@ -15,6 +15,16 @@ const getChildrenNodes = (id) => (state) =>
       )
     : state.nodes.filter(({ parentId }) => id === parentId)
 
+const getDescendantNodes = (id) => (state) =>
+  getChildrenNodes(id)(state).reduce(
+    (descendants, node) =>
+      descendants.concat(node).concat(getDescendantNodes(node.id)(state)),
+    []
+  )
+
+const getDescendantNodeIds = (id) => (state) =>
+  getDescendantNodes(id)(state).map(({ id }) => id)
+
 const getIsContainer = (id) => (state) =>
   id === rootId || getNodeById(id)(state)?.isContainer === true
 
@@ -41,28 +51,58 @@ const getRenderBody = (id) => (state) => {
 }
 
 export const createFlowViewStore = (nodes) =>
-  zustand((set, get) => ({
+  zustand((set) => ({
     nextId: rootId + 1,
     nodes,
-    setSelectedNodes: (selectedNodeIds, selected) => () =>
+    setSelectedNodes: (nodeIds, selected) =>
       set((state) => ({
-        ...state,
-        nodes: get().nodes.map(({ id, ...node }) =>
-          selectedNodeIds.includes(id)
-            ? { ...node, id, selected }
-            : { ...node, id }
+        // 1. First remove the `selected` attribute from nodes that are not involved.
+        nodes: state.nodes
+          .filter(({ id }) => !nodeIds.includes(id))
+          .map(({ selected, ...node }) => node)
+          .concat(
+            // 2. Then move selected nodes to the end of the list in order to paint them on top.
+            state.nodes
+              .filter(({ id }) => nodeIds.includes(id))
+              .map((node) => ({ ...node, selected }))
+          ),
+      })),
+    setStartDraggingPoint: (startDraggingPoint) =>
+      set((state) => ({ startDraggingPoint })),
+    translateNodes: (nodeIds, draggingDelta) =>
+      set((state) => ({
+        nodes: state.nodes.map(({ id, position, ...node }) =>
+          nodeIds.includes(id)
+            ? {
+                ...node,
+                id,
+                position: {
+                  x: position.x + draggingDelta.x,
+                  y: position.y + draggingDelta.y,
+                },
+              }
+            : { ...node, id, position }
         ),
       })),
   }))
 
 export function FlowViewNode({ id = rootId, parentId = rootId, useStore }) {
+  const isRoot = id === rootId
+
   const { dimension, position, noFootbar, noHeadbar, selected } = useStore(
     getNodeById(id)
   )
-  const toggleSelection = useStore((state) =>
-    state.setSelectedNodes([id], !selected)
-  )
+  const descendantNodeIds = useStore(getDescendantNodeIds(id))
+  const isContainer = useStore(getIsContainer(id))
   const renderBody = useStore(getRenderBody(id))
+  const selectedNodes = useStore((state) =>
+    state.nodes.filter(({ selected }) => selected)
+  )
+  const selectedNodesIds = selectedNodes.map(({ id }) => id)
+  const setSelectedNodes = useStore((state) => state.setSelectedNodes)
+  const setStartDraggingPoint = useStore((state) => state.setStartDraggingPoint)
+  const startDraggingPoint = useStore((state) => state.startDraggingPoint)
+  const translateNodes = useStore((state) => state.translateNodes)
 
   return (
     <div
@@ -71,8 +111,43 @@ export function FlowViewNode({ id = rootId, parentId = rootId, useStore }) {
       })}
       onClick={(event) => {
         event.stopPropagation()
+      }}
+      onMouseDown={(event) => {
+        event.stopPropagation()
 
-        toggleSelection()
+        setStartDraggingPoint({ x: event.clientX, y: event.clientY })
+
+        if (isRoot) {
+          setSelectedNodes(descendantNodeIds, false)
+        } else {
+          setSelectedNodes(selectedNodesIds, false)
+          setSelectedNodes([id], true)
+        }
+      }}
+      onMouseLeave={() => {
+        if (isContainer) {
+          setStartDraggingPoint()
+        }
+      }}
+      onMouseMove={(event) => {
+        if (isContainer) {
+          event.stopPropagation()
+
+          if (startDraggingPoint) {
+            const { clientX: x, clientY: y } = event
+
+            translateNodes(selectedNodesIds, {
+              x: x - startDraggingPoint.x,
+              y: y - startDraggingPoint.y,
+            })
+            setStartDraggingPoint({ x, y })
+          }
+        }
+      }}
+      onMouseUp={(event) => {
+        event.stopPropagation()
+
+        setStartDraggingPoint()
       }}
       style={{ ...dimension, top: position.y, left: position.x }}
     >
